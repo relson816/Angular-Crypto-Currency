810d59d7dce6989cb3018d7394d214cba62f3f60
relson816 <relson.batayola@gmail.com>
2018-02-27 14:17:37 +0800

    Connected to API

---
+++ src/app/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class DataService {
+
+  result:any;
+   constructor(private _http: Http) {}
+
+  getPrices() {
+    return this._http.get("https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD")
+      .map(result => this.result = result);
+  }
+
+}
